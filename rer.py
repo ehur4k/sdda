0151f19c7c0c18f381b769c83cd4cc222e314ce5
ehur4k <129819204+ehur4k@users.noreply.github.com>
2023-04-16 09:49:41 +0800

    Create rer.py

---
+++ rer.py
@@ -0,0 +1,21 @@
+while True:
+   print("選項:")
+   print("輸入 '+' 讓兩個數值互加")
+   print("輸入 '-' 讓兩個數值互減")
+   print("輸入 '*' 讓兩個數值互乘")
+   print("輸入 '/' 讓兩個數值互除")
+   print("輸入 'exit' 來結束程式")
+   user_input = input(": ")
+
+   if user_input == "exit":
+      break
+   elif user_input == "+":
+      ...
+   elif user_input == "-":
+      ...
+   elif user_input == "*":
+      ...
+   elif user_input == "/":
+      ...
+   else:
+      print("不知道你在輸入什麼")  
